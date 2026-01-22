49852d2afd274472451e8c4e2bd7433b1e2f2bd7
Lindzey Coonradt <coonradt5678@gmail.com>
2026-01-21 18:00:34 -0700

    Icons + styling

---
+++ script.js
@@ -0,0 +1,7 @@
+function visitLinkedIn() {
+    window.open('https://www.linkedin.com/in/lindzey-coonradt-2530902b8', '_blank')
+}
+
+function visitGithub() {
+    window.open('https://github.com/lindzc15', '_blank')
+}
